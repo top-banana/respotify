46f8e3173b4df87c27a24d2a45b6a60565c53520
Nitin Punjabi <nitin@patternhatch.com>
2016-07-05 20:57:28 -0400

    Integrate Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+import React from "react";
+import ReactDOM from "react-dom";
+import Greeting from './greeting';
+ 
+ReactDOM.render(
+  <Greeting name="Nitin"/>,
+  document.getElementById('container')
+);
